bb9a391164462651619bd00b1e1b76da3e4ab59a
Akanksha Singh <akanksha.singh1@hashedin.com>
2020-06-11 14:10:31 +0530

    added extension

---
+++ vs code extension/case-change/README.md
@@ -0,0 +1,35 @@
+# Change Case
+
+This extension allows you to convert the selected text into different case. Supported Cases are:
+
+1. Snake Case
+2. Camel Case
+3. Kebab Case
+4. Upper Case
+5. Lower Case
+
+## **How to Use it**
+
+**1. Select any text from the file, You will see an left side icon for options**\
+\
+![Alt text](image1.png?raw=true "Title") \
+**2. Click on the icon, it will give the options on chnage the case**\
+\
+![Alt text](image2.png?raw=true "Title")\
+**3. Select any option from the dropdown**\
+\
+![Alt text](image3.png?raw=true "Title")\
+**4. The selected text case will gets changed**\
+\
+![Alt text](image4.png?raw=true "Title")
+\
+
+> Language Supported: javascript, typescript, javascriptreact, typescriptreact, html, css, less, scss, sass, python, json, markdown
+
+## Release Notes
+
+### 1.0.0
+
+Initial release of Change Case
+
+**Enjoy!**
---
+++ vs code extension/case-change/src/extension.ts
@@ -0,0 +1,154 @@
+import * as vscode from "vscode";
+
+const COMMAND = "case-change";
+
+export function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.languages.registerCodeActionsProvider(
+      [
+        { scheme: "file", language: "javascript" },
+        { scheme: "file", language: "typescript" },
+        { scheme: "file", language: "html" },
+        { scheme: "file", language: "css" },
+        { scheme: "file", language: "less" },
+        { scheme: "file", language: "typescriptreact" },
+        { scheme: "file", language: "scss" },
+        { scheme: "file", language: "python" },
+        { scheme: "file", language: "markdown" },
+        { scheme: "file", language: "json" },
+        { scheme: "file", language: "javascriptreact" },
+        { scheme: "file", language: "sass" },
+      ],
+      new ChangeCase(),
+      {
+        providedCodeActionKinds: ChangeCase.providedCodeActionKinds,
+      }
+    )
+  );
+}
+
+export class ChangeCase implements vscode.CodeActionProvider {
+  public static readonly providedCodeActionKinds = [
+    vscode.CodeActionKind.QuickFix,
+  ];
+
+  public provideCodeActions(
+    document: vscode.TextDocument,
+    range: vscode.Range
+  ): vscode.CodeAction[] | undefined {
+    var editor = vscode.window.activeTextEditor;
+    if (!editor) {
+      return; // No open text editor
+    }
+
+    var selection = editor.selection;
+    var text = editor.document.getText(selection);
+
+    const changeToSnakeCase = this.createFix(
+      document,
+      range,
+      "Snake Case",
+      text
+    );
+    const changeToCamelCase = this.createFix(
+      document,
+      range,
+      "Camel Case",
+      text
+    );
+
+    const changeToCapitalCase = this.createFix(
+      document,
+      range,
+      "Capital Case",
+      text
+    );
+
+    const changeToLowerCase = this.createFix(
+      document,
+      range,
+      "Lower Case",
+      text
+    );
+
+    const changeToKebabCase = this.createFix(
+      document,
+      range,
+      "Kebab Case",
+      text
+    );
+    changeToCamelCase.isPreferred = true;
+    return [
+      changeToCamelCase,
+      changeToSnakeCase,
+      changeToKebabCase,
+      changeToCapitalCase,
+      changeToLowerCase,
+    ];
+  }
+
+  toSnakeCase(text: string) {
+    // @ts-ignore
+    var snakeCaseText = text
+      .match(
+        /[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g
+      )
+      .map((x) => x.toLowerCase())
+      .join("_");
+    return snakeCaseText;
+  }
+
+  toCamelCase(text: string) {
+    var camelCaseText = text
+      .toLowerCase()
+      .replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase());
+    return camelCaseText;
+  }
+
+  toKebabCase(text: string) {
+    // @ts-ignore
+    var kebabCaseText = text
+      .match(
+        /[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g
+      )
+      .map((x) => x.toLowerCase())
+      .join("-");
+    return kebabCaseText;
+  }
+
+  mapFunctionToArguments(emoji: string, text: string) {
+    switch (emoji) {
+      case "Snake Case":
+        return this.toSnakeCase(text);
+      case "Camel Case":
+        return this.toCamelCase(text);
+      case "Kebab Case":
+        return this.toKebabCase(text);
+      case "Capital Case":
+        return text.toUpperCase();
+      case "Lower Case":
+        return text.toLowerCase();
+    }
+  }
+
+  private createFix(
+    document: vscode.TextDocument,
+    range: vscode.Range,
+    emoji: string,
+    text: string
+  ): vscode.CodeAction {
+    const fix = new vscode.CodeAction(
+      `Change Case to ${emoji}`,
+      vscode.CodeActionKind.QuickFix
+    );
+    fix.edit = new vscode.WorkspaceEdit();
+    fix.edit.replace(
+      document.uri,
+      new vscode.Range(range.start, range.end),
+      // @ts-ignore
+      this.mapFunctionToArguments(emoji, text)
+    );
+
+    return fix;
+  }
+}
